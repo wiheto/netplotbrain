a610c0bcf072069805efbe8e7dfe6fa6721e499f
William Hedley Thompson <wiheto@users.noreply.github.com>
2021-08-27 13:09:15 +0200

    Update create_gif.py
    Removing trailing white spaces

--- netplotbrain/create_gif.py
+++ netplotbrain/create_gif.py
@@ -1,28 +1,28 @@
-import io       
-from PIL import Image 
+import io
+from PIL import Image
+
 
 def create_gif(fig, ax, gifname='netplotbrain.gif'):
-    
     """
     Create a GIF from plot
-    
+
     Requires the function 'plot' to contain the paramater 'frames' (two or more frames needed)
     Takes return values from the function 'plot'
-    
+
     Parameters
     ---------------------
 
     ax : list of subplot axes
-    
+
     fig : matplotlib figure
-    
+
     gifname: str (default 'netplotbrain.gif')
     Name of the saved GIF
- 
+
     """
-    
+
     images = []
-    
+
     for i, currax in enumerate(ax):
         extent = currax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
         buf = io.BytesIO()
@@ -30,21 +30,8 @@ def create_gif(fig, ax, gifname='netplotbrain.gif'):
         buf.seek(i)
         img = Image.open(buf).convert('RGBA')
         images.append(img)
-        
-    del images[(len(images)-1)//2] 
-        
+
+    del images[(len(images)-1)//2]
+
     images[0].save(gifname,
-                   save_all=True, append_images=images[1:], optimize=False, duration=600, loop=0) #duration = each frame in ms 
-
-
-
-
-
-
-
-
-
-    
-    
-      
-   
+                   save_all=True, append_images=images[1:], optimize=False, duration=600, loop=0)  # duration = each frame in ms
