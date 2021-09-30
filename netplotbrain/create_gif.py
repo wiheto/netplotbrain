9ee8d04b942fb19d1f46d7679b5ce70c3e167736
silviafan <66485342+silviafan@users.noreply.github.com>
2021-09-30 10:50:56 +0200

    Update create_gif.py

--- netplotbrain/create_gif.py
+++ netplotbrain/create_gif.py
@@ -1,7 +1,7 @@
 import io       
 from PIL import Image 
 
-def create_gif(fig, ax, gifname='netplotbrain.gif'):
+def create_gif(fig, ax_out, duration, loop, gifname='netplotbrain.gif'):
     
     """
     Create a GIF from plot
@@ -16,14 +16,21 @@ def create_gif(fig, ax, gifname='netplotbrain.gif'):
     
     fig : matplotlib figure
     
+    duration: each frame in ms, int (600 is suggested)
+    
+    loop: number of loops, int
+    If 0, it becomes an infinite loop
+    
     gifname: str (default 'netplotbrain.gif')
     Name of the saved GIF
  
     """
     
+    #saving matplotlib figures (frame images) to buffer and opening in PIL Image objects
+    
     images = []
     
-    for i, currax in enumerate(ax):
+    for i, currax in enumerate(ax_out):
         extent = currax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
         buf = io.BytesIO()
         fig.savefig(buf, bbox_inches=extent, format='PNG', dpi=300)
@@ -31,10 +38,8 @@ def create_gif(fig, ax, gifname='netplotbrain.gif'):
         img = Image.open(buf).convert('RGBA')
         images.append(img)
         
-    del images[(len(images)-1)//2] 
-        
     images[0].save(gifname,
-                   save_all=True, append_images=images[1:], optimize=False, duration=600, loop=0) #duration = each frame in ms 
+                   save_all=True, append_images=images[1:], optimize=False, duration=duration, loop=loop) 
 
 
 
