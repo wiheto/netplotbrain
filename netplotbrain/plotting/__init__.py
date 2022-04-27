b43255ada489a735c587e04e3de2c57502c65611
Silvia Fanton <66485342+silviafan@users.noreply.github.com>
2022-04-27 08:20:00 +0200

    code back to original

--- netplotbrain/plotting/__init__.py
+++ netplotbrain/plotting/__init__.py
@@ -6,7 +6,7 @@ from .plot_templates import _plot_template, _plot_template_style_filled, \
     _plot_template_style_cloudy, _plot_template_style_surface
 from .plot_dimarrows import _add_axis_arrows
 from .plot_parcels import _get_nodes_from_nii, _plot_parcels
-from .plot_title import _add_subplot_title, _title_align
+from .plot_title import _add_subplot_title
 from .process_input import get_frame_input, _process_edge_input, _process_node_input,\
     _init_figure, _check_axinput
 from .plot_legend import _add_nodecolor_legend, _add_nodesize_legend, _setup_legend
@@ -17,6 +17,6 @@ __all__ = ['_plot_template', '_plot_template_style_filled',
            '_scale_nodes', '_plot_nodes', '_plot_spheres',
            '_plot_edges', '_plot_template_style_surface',
            '_add_axis_arrows', '_get_nodes_from_nii', '_plot_parcels',
-           '_select_single_hemisphere_nodes', '_npedges2dfedges', '_add_subplot_title', '_title_align', 
+           '_select_single_hemisphere_nodes', '_npedges2dfedges', '_add_subplot_title', 
            '_setup_legend', '_process_edge_input', '_process_node_input', '_add_nodesize_legend',
            '_add_nodecolor_legend', 'get_frame_input', '_init_figure', '_check_axinput', '_plot_gif']
--- netplotbrain/plotting/plot_title.py
+++ netplotbrain/plotting/plot_title.py
@@ -1,5 +1,4 @@
 
-
 def _add_subplot_title(ax, azim=None, elev=None, title='auto', hemisphere='both', **kwargs):
     titlefont = kwargs.get('font')
     titlecolor = kwargs.get('fontcolor')
@@ -31,82 +30,4 @@ def _add_subplot_title(ax, azim=None, elev=None, title='auto', hemisphere='both'
                      fontweight=titleweight, color=titlecolor,
                      fontsize=titlefontsize,
                      loc=titleloc)
-
-def _title_align(ax, fig, azim=None, elev=None, titlealign='L', title='auto', hemisphere='both', **kwargs):
-    titlefont = kwargs.get('font')
-    titlecolor = kwargs.get('fontcolor')
-    titlefontsize = kwargs.get('titlefontsize')
-    titleloc = kwargs.get('titleloc')
-    titleweight = kwargs.get('titleweight')
-    viewcoord = (azim, elev)
-    if title == 'auto':
-        title = ''
-        if viewcoord == (180, 10):
-            title = 'Left'
-        elif viewcoord == (0, 10):
-            title = 'Right'
-        elif viewcoord == (90, 10):
-            title = 'Anterior'
-        elif viewcoord == (-90, 10):
-            title = 'Posterior'
-        elif viewcoord == (-90, 90):
-            title = 'Superior'
-        elif viewcoord == (90, 90):
-            title = 'Inferior'
-        # Add hemisphere
-        if hemisphere == 'L' or hemisphere == 'left':
-            title += ' (left hemisphere)'
-        elif hemisphere == 'R' or hemisphere == 'right':
-            title += ' (right hemisphere)'
-
-    if titlealign is None:
-        fig.suptitle(title)
-    elif titlealign == 'L':
-        if viewcoord == (180, 10):  
-            ax.set_title(title, fontname=titlefont,
-                             fontweight=titleweight, color=titlecolor,
-                             fontsize=titlefontsize,
-                             loc=titleloc)
-        else:
-            ax.set_title('')
-    elif titlealign == 'R':
-        if viewcoord == (0, 10):  
-            ax.set_title(title, fontname=titlefont,
-                             fontweight=titleweight, color=titlecolor,
-                             fontsize=titlefontsize,
-                             loc=titleloc)
-        else:
-            ax.set_title('')
-    elif titlealign == 'A':
-        if viewcoord == (90, 10):  
-            ax.set_title(title, fontname=titlefont,
-                             fontweight=titleweight, color=titlecolor,
-                             fontsize=titlefontsize,
-                             loc=titleloc)
-        else:
-            ax.set_title('')
-    elif titlealign == 'P':
-        if viewcoord == (-90, 10):  
-            ax.set_title(title, fontname=titlefont,
-                             fontweight=titleweight, color=titlecolor,
-                             fontsize=titlefontsize,
-                             loc=titleloc)
-        else:
-            ax.set_title('')
-    elif titlealign == 'S':
-        if viewcoord == (-90, 90):  
-            ax.set_title(title, fontname=titlefont,
-                             fontweight=titleweight, color=titlecolor,
-                             fontsize=titlefontsize,
-                             loc=titleloc)
-        else:
-            ax.set_title('')
-    elif titlealign == 'I':
-        if viewcoord == (90, 90):  
-            ax.set_title(title, fontname=titlefont,
-                             fontweight=titleweight, color=titlecolor,
-                             fontsize=titlefontsize,
-                             loc=titleloc)
-        else:
-            ax.set_title('')
-           
+    
