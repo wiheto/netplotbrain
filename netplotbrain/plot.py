65046acd2bbc4b17940c54b733c12715204049c5
xthowi <xthowi@pop-os.localdomain>
2022-06-20 09:06:01 +0200

    fixing savefig error

--- netplotbrain/plot.py
+++ netplotbrain/plot.py
@@ -235,11 +235,11 @@ def plot(nodes=None, fig: Optional[plt.Figure] = None, ax=None, view: str = 'L',
         _plot_gif(fig, ax_out, profile['gifduration'], profile['savename'], profile['gifloop'])
     elif profile['savename'] is not None:
         if profile['savename'].endswith('.png'):
-            fig.savedfig(profile['savename'], dpi=profile['fig_dpi'])
+            fig.savefig(profile['savename'], dpi=profile['fig_dpi'])
         elif profile['savename'].endswith('.svg'):
-            fig.savedfig(profile['savename'])
+            fig.savefig(profile['savename'])
         else:
-            fig.savedfig(profile['savename'] + '.png', dpi=profile['fig_dpi'])
-            fig.savedfig(profile['savename'] + '.svg')
+            fig.savefig(profile['savename'] + '.png', dpi=profile['fig_dpi'])
+            fig.savefig(profile['savename'] + '.svg')
 
     return (fig, ax_out)
