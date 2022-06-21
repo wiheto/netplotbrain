ef0d18dca0ca9975f723007181cf896ed56f8816
wiheto <william.thompson@gu.se>
2022-06-21 13:30:11 +0200

    removing doc example data and using it in examples instead

--- docs/gallary/simple.py
+++ docs/gallary/simple.py
@@ -17,8 +17,8 @@ import netplotbrain
 import pandas as pd
 
 # Example node and edges dataframes included with package
-nodes = pd.read_csv('./example_nodes.tsv', sep='\t', index_col=0)
-edges = pd.read_csv('./example_edges.tsv', sep='\t', index_col=0)
+nodes = pd.read_csv('./examples/example_nodes.tsv', sep='\t', index_col=0)
+edges = pd.read_csv('./examples/example_edges.tsv', sep='\t', index_col=0)
 
 # Plot single view
 netplotbrain.plot(template='MNI152NLin2009cAsym',
